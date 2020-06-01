e1332a8ec1e90053f49936c7d84b957fbf9d9e17
DanielRangel <danf.arangel@gmail.com>
2020-05-31 23:40:25 -0300

    adicionado data.json, fs, e desconstrucao de array

--- modulo04/Instructors.js
+++ modulo04/Instructors.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 exports.post = function(req,res){
-    
+    /* vlaidação */
     const keys = Object.keys(req.body)
     
     for(key of keys){
@@ -7,6 +10,24 @@ exports.post = function(req,res){
             return res.send("por favor, preencha todos os campos")
         }
     }
-    
-    return res.send(keys)
+    /* descontrução de array */
+    let {avatar_url, name, birth, gender, services} = req.body
+    /* tratamento de dados adicionando mais informação */
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+    /* organizando dados para ser enviados */
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("erro ao escrever")
+        return res.redirect("/instructors")
+    })
 }
