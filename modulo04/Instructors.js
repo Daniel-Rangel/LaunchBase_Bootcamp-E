b8a6d183fa658d51b0d5482a430409376d442f06
DanielRangel <danf.arangel@gmail.com>
2020-06-03 01:10:23 -0300

    add logica de birth e pagina show.njk

--- modulo04/Instructors.js
+++ modulo04/Instructors.js
@@ -1,6 +1,44 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+//show
+exports.show = function(req, res){
+    const { id } = req.params
+
+    const foudInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if(!foudInstructor) return res.send("instructor not found")
+
+    function age(timestamp) {
+        const today = new Date() // dia atual
+        const brithDate = new Date(timestamp) // dia do anivesario
+    
+        //informa quando anos tem
+        let age = today.getFullYear() - brithDate.getFullYear()
+        const month = today.getMonth() - brithDate.getMonth()
+        
+        if (month < 0 || 
+            month == 0 && 
+            today.getDate() <= brithDate.getDate()){
+            age = age -1
+        }
+        
+        return age
+    }
+
+    const instructor = {
+        ...foudInstructor,
+        age: age(foudInstructor.birth),
+        services: foudInstructor.services.split(','),
+        created_at:''
+    }
+
+
+    return res.render("instructors/show", { instructor  })
+}
+
 exports.post = function(req,res){
     /* vlaidação */
     const keys = Object.keys(req.body)
