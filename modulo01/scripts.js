b886db0a072a88316d4d233c855c90a53448e732
DanielRangel <danf.arangel@gmail.com>
2020-11-10 18:39:42 -0300

    adicionado modulo js

---
+++ modulo01/scripts.js
@@ -0,0 +1,22 @@
+//Basico 
+
+//Faxendo calculos 
+const aluno1 = "Lucas"
+const aluno2 = "Daniel"
+const aluno3 = "thamires"
+
+const notaAluno1 = 9.8
+const notaAluno2 = 10
+const notaAluno3 = 7.3
+
+const media = (notaAluno1 + notaAluno2 + notaAluno3) / 3
+
+const texto = `
+    ${aluno1} sua nota é de ${notaAluno1}, \n 
+    ${aluno2} nota ${notaAluno2}, \n
+    ${aluno3} nota ${notaAluno3}.\n\n
+    A media da Sala é ${media}.
+
+    `
+console.log(texto)
+
