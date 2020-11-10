b046de9f43caf77c4031b8d87930b715979c218f
DanielRangel <danf.arangel@gmail.com>
2020-11-10 19:35:55 -0300

    rocket/js md1

--- modulo01/scripts.js
+++ modulo01/scripts.js
@@ -2,21 +2,31 @@
 
 //Faxendo calculos 
 const aluno1 = "Lucas"
-const aluno2 = "Daniel"
-const aluno3 = "thamires"
+const notaAluno1 = 5.0
 
-const notaAluno1 = 9.8
-const notaAluno2 = 10
-const notaAluno3 = 7.3
+const aluno2 = "Daniel"
+const notaAluno2 = 7
+
+const aluno3 = "thamires"
+const notaAluno3 = 2.3
+
 
 const media = (notaAluno1 + notaAluno2 + notaAluno3) / 3
 
 const texto = `
-    ${aluno1} sua nota é de ${notaAluno1}, \n 
-    ${aluno2} nota ${notaAluno2}, \n
-    ${aluno3} nota ${notaAluno3}.\n\n
-    A media da Sala é ${media}.
-
+    ${aluno1} sua nota é de ${notaAluno1},
+    ${aluno2} nota ${notaAluno2}, 
+    ${aluno3} nota ${notaAluno3}.
+    A media da Sala é ${media.toFixed(2)}.
     `
-console.log(texto)
+//console.log(texto)
+
+/* condicionais */
+/* if(media > 5){
+    console.log(`Media é de ${media.toFixed(3)} Parabens`)
+}else{
+    console.log(`Media é menor que 5`)
+} */
+
+//Operadores relacionais e comparativos
 
