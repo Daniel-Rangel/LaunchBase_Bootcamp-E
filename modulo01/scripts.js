2f59eb93b6c8023d47dc6d29a5099b8d0c3ace92
DanielRangel <danf.arangel@gmail.com>
2020-11-12 20:55:24 -0300

    modulo2 desafio 1 js

--- modulo01/scripts.js
+++ modulo01/scripts.js
@@ -50,3 +50,30 @@ if(!(idade > 18) || idade === 17){
 }
 
  */
+
+/*  const nome = "daniel"
+ const peso = 80
+ const altura = 1.74
+
+ const imc = peso / (altura * altura )
+
+ if(imc >= 30){
+     console.log(`${nome} você está acima do peso`)
+ }else if(imc < 29.9){
+     console.log(`${nome} você não está acima do peso`)
+ }
+ */
+ /* apoosentadoria calc */
+
+ const nome1 = "Silva"
+ const sexo = "F"
+ const idade = 48
+ const contribuicao = 23
+
+
+if(sexo == "F" && (contribuicao + idade) >= 85){
+    console.log(`${nome1},você pode se aposentar!`)
+}else{
+    console.log(`${nome1},você não pode se aposentar!`)
+}
+
